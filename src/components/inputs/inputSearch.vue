5c0fa2b9271f489fabe0204742373804ded11451
danielcrubens <dcr.tecnologi@gmail.com>
2023-04-06 09:13:41 -0300

    componente InputSearch :memo:

---
+++ src/components/inputs/inputSearch.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="relative text-white">
+    <input type="text" class="w-full bg-gray-700 placeholder-white px-4 py-2 rounde-lg opacity-30 outline-none focus:outline-none focus:opacity-60 pr-10" placeholder="Search">
+    <SearchIcon class="w-4 h-4 absolute text-gray-500 top-2 right-2"/>
+  </div>
+</template>
+
+<script setup>
+import SearchIcon from '../icons/SearchIcon.vue'
+
+</script>
--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="bg-gray-800 flex text-white max-h-screen h-screen ">
+  <div class="bg-gray-800 px-12 flex text-white max-h-screen h-screen ">
     <div class=" w-64">
       <span class="font-semibold p-6 block opacity-90">Skateboard</span>
-
       <nav class="p-6">
         <h3 class="opacity-50 uppercase text-xs font-semibold -tracking-wider mb-6">Menu</h3>
         <div class="space-y-6">
@@ -16,7 +15,21 @@
         </div>
       </nav>
     </div>
-    <div class="border-2 flex-1 border-red-500">Content</div>
+    <div class=" p-5 flex-1">
+      <header>
+        <InputSearch />
+<div class="flex items-center space-x-3">
+       <!--  <Dropdown> -->
+          <Avatar />
+          <span>Thomas</span>
+          <ChevronDownIcon class="w-4 h-4"/>
+        <!-- </Dropdown> -->
+      </div>
+
+        <!-- <Notifications/> --> 
+
+      </header>
+    </div>
   </div>
 </template>
 
@@ -25,23 +38,6 @@
 import MenuItem from './navs/MenuItem.vue';
 import HomeIcon from '../components/icons/HomeIcon.vue';
 import TrendingUpIcon from '../components/icons/TrendingUpIcon.vue';
-
-
-/* export default {
-  components:{
-    MenuItem,
-    TrendingUpIcon,
-    HomeIcon,    
-  },
-  data() {
-    return {
-      menuItems: [
-        { name: "Home", icon: HomeIcon },
-        { name: "Trending", icon: TrendingUpIcon },
-        // Add more menu items here...
-      ]
-    }
-  },
-  } */
+import InputSearch from '../components/inputs/inputSearch.vue'
 
 </script>
