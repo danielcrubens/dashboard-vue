f7f50f5523534d32431aa05105a74a3589e89900
danielcrubens <dcr.tecnologi@gmail.com>
2023-04-09 17:37:33 -0300

    componente DarkMode :memo:

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="bg-gray-800 px-2 flex text-white max-h-screen h-screen overflow-y-auto overflow-x-hidden">
-    <div class=" w-64">
+  <main class="bg-white dark:bg-gray-800 dark:text-white text-gray-800 px-2 flex  max-h-screen h-screen overflow-y-auto overflow-x-hidden">
+    <div class=" w-64 sticky top-0 z-20">
       <span class="font-semibold p-6 block opacity-90">Skateboard</span>
       <nav class="p-6">
         <h3 class="opacity-50 uppercase text-xs font-semibold -tracking-wider mb-6">Menu</h3>
         <div class="space-y-6">
-          <MenuItem :icon="HomeIcon">Dicover</MenuItem>
+          <MenuItem :icon="HomeIcon" first>Dicover</MenuItem>
           <MenuItem :icon="TrendingUpIcon">Trending </MenuItem>
-          <MenuItem :icon="TrendingUpIcon">Trending </MenuItem>
-          <MenuItem :icon="TrendingUpIcon">Trending </MenuItem>
-          <MenuItem :icon="TrendingUpIcon">Trending </MenuItem>
+          <MenuItem :icon="StreamingIcon">Streaming </MenuItem>
+          <MenuItem :icon="Playlist">Playlist </MenuItem>
+          <MenuItem :icon="Bookmark">Bookmark </MenuItem>
 
+          <hr class="w-auto color-red-600 space-y-28">
+          <h3 class="opacity-50 uppercase text-xs font-semibold -tracking-wider mb-6">Categoty</h3>
 
-        </div>
+          <MenuItem :icon="StreamingIcon">Streaming </MenuItem>
+          <MenuItem :icon="Playlist">Playlist </MenuItem>
+          
+          <DarkMode/>
+
+        </div> 
       </nav>
     </div>
     <div class=" p-5 flex-1 h-screen relative ">
-      <header class="flex p-5 bg-gray-800 sticky top-0 z-20 items-center justify-between">
+      <header class="flex p-5 bg-white dark:bg-gray-800 sticky top-0 z-20 items-center justify-between">
         <InputSearch class="w-96" />
 
         <div class="flex items-center">
@@ -30,38 +37,38 @@
           <BellIcon class="w-5 h-5 ml-8 opacity-50 hover:opacity-100 cursor-pointer transition duration-100" />
           <Notifications />
         </div>
-      </header>
-<div class="px-5 pb-5">
-      <h1 class="font-bold text-4xl mt-6 mb-4">Discover</h1>
-      <div class="grid gap-4 grid-cols-12">
-        <div class="col-span-8 h-96 rounded-3xl overflow-hidden relative">
-          <div class=" flex w-full h-full">
-            <img class="object-cover w-full h-full" src="https://picsum.photos/2000/1400" alt="Imagem">
+      </header> 
+      <div class="px-5 pb-5">
+        <h1 class="font-bold text-4xl mt-6 mb-4">Discover</h1>
+        <div class="grid gap-4 grid-cols-12">
+          <div class="col-span-8 h-96 rounded-3xl overflow-hidden relative">
+            <div class=" flex w-full h-full">
+              <img class="object-cover w-full h-full" src="https://picsum.photos/2000/1400" alt="Imagem">
+            </div>
+            <div class="z-10 absolute top-0 p-6 w-full bottom-0">
+              <h3 class="text-2xl max-w-xs">How to do Basic Jumping and how to landing safely</h3>
+              <div class="absolute right-6 bottom-6 px-2 py-1 rounded-lg bg-gray-800 bg-opacity-50 text-white">7 minutes
+              </div>
+            </div>
           </div>
-          <div class="z-10 absolute top-0 p-6 w-full bottom-0">
-            <h3 class="text-2xl max-w-xs">How to do Basic Jumping and how to landing safely</h3>
-            <div class="absolute right-6 bottom-6 px-2 py-1 rounded-lg bg-gray-800 bg-opacity-50 text-white">7 minutes
+          <div class="col-span-4 h-96 rounded-3xl overflow-hidden relative">
+            <div class=" flex w-full h-full">
+              <img class="object-cover w-full h-full" src="https://picsum.photos/2000/1400" alt="Imagem">
+            </div>
+            <div class="z-10 absolute top-0 p-6 w-full bottom-0">
+              <h3 class="text-2xl max-w-xs">How to do Basic Jumping and how to landing safely</h3>
+              <div class="absolute right-6 bottom-6 px-2 py-1 rounded-lg bg-gray-800 bg-opacity-50 text-white">7 minutes
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-span-4 h-96 rounded-3xl overflow-hidden relative">
-          <div class=" flex w-full h-full">
-            <img class="object-cover w-full h-full" src="https://picsum.photos/2000/1400" alt="Imagem">
-          </div>
-          <div class="z-10 absolute top-0 p-6 w-full bottom-0">
-            <h3 class="text-2xl max-w-xs">How to do Basic Jumping and how to landing safely</h3>
-            <div class="absolute right-6 bottom-6 px-2 py-1 rounded-lg bg-gray-800 bg-opacity-50 text-white">7 minutes
-            </div>
-          </div>
+        <h1 class=" text-2xl mt-6 mb-4">Most Watched</h1>
+        <div class="grid grid-cols-4 gap-4">
+          <CardItem v-for="n  of 4 " :key="n" />
         </div>
-      </div>
-      <h1 class=" text-2xl mt-6 mb-4">Most Watched</h1>
-      <div class="grid grid-cols-4 gap-4">
-        <CardItem v-for="n  of 4 " />
       </div>
     </div>
-  </div>
-</div>
+  </main>
 </template>
 
 <script setup>
@@ -74,4 +81,8 @@ import Avatar from '../components/Avatar.vue'
 import ChevronDownIcon from '../components/icons/ChevronDownIcon.vue'
 import BellIcon from '../components/icons/BellIcon.vue'
 import CardItem from '../components/CardItem.vue'
+import StreamingIcon from '../components/icons/StreamingIcon.vue'
+import Playlist from '../components/icons/Playlist.vue'
+import Bookmark from '../components/icons/Bookmark.vue'
+import DarkMode from './navs/DarkMode.vue'
 </script>
--- src/layouts/navs/MenuItem.vue
+++ src/layouts/navs/MenuItem.vue
@@ -1,17 +1,37 @@
 <template>
-  <div  class=" flex items-center space-x-4">
-            <div class="bg-orange-500 rounded-lg p-2">
-              <component :is="icon" class="w-4 h-4" />
-            </div>
-            <span>
-              <slot></slot>
-            </span>
-          </div>
+  <div class="flex items-center space-x-4">
+    <div :class="[first ? 'bg-orange-500' : isActive ? 'bg-orange-500' : 'bg-gray-700','rounded-lg p-2']" @mouseenter="hoverActive" @mouseleave="hoverInactive">
+      <component :is="icon" class="w-4 h-4" />
+    </div>
+    <span>
+      <slot></slot>
+    </span>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from "vue";
-defineProps({
-  icon: {default: null}
+import { defineProps,defineExpose, ref } from 'vue';
+
+const props = defineProps({
+  icon: { default: null },
+  first: { type: Boolean, default: false },
 });
+
+const isActive = ref(props.first);
+
+function hoverActive() {
+  isActive.value = true;
+}
+
+function hoverInactive() {
+  isActive.value = props.first;
+}
+
+defineExpose({
+  isActive,
+  hoverActive,
+  hoverInactive,
+  props,
+});
+
 </script>
